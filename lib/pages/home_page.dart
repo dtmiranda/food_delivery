f5cbe032215219f3e687a3baf8011e52a07b4c32
samuel.miranda <samuel.miranda@buscamilhas.com>
2024-04-20 19:44:55 -0100

    feat: create silver app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery/components/my_drawer.dart';
+import 'package:food_delivery/components/my_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +14,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
+    return Scaffold(  
       drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                )
+                //my current location
+
+                //description box
+
+              ],
+            ),
+            title: Text("title")
+          )
+        ],
+        body: Container(color: Colors.blue),
+        
+      ),
     );
   }
 }

f9d94254184e6170ad325d740082330320ae16e6
samuel.miranda <samuel.miranda@buscamilhas.com>
2024-04-20 15:38:58 -0100

    feat: create login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:food_delivery/components/my_button.dart';
+import 'package:food_delivery/components/my_textfield.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget{
+  final void Function()? onTap;
+  
+  const LoginPage({super.key, required this.onTap});
+  
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+        
+            const SizedBox(height: 25),
+
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+
+            MyTextField(
+              hintText: "Email", 
+              obscureText: false, 
+              controller: emailController
+            ),
+
+            const SizedBox(height: 10),
+
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: passwordController
+            ),
+
+            const SizedBox(height: 10),
+
+            MyButton(
+              text: "Sign In", 
+              onTap: () {}
+            ),
+
+            const SizedBox(height: 25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Register now", 
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 
